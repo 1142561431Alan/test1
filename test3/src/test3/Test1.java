9c0ded47d2819b028a6645b36bec9d6ffc6f3163
wanght <wanght@DESKTOP-0067PD9>
2019-05-30 16:01:54 +0800

    2

--- test3/src/test3/Test1.java
+++ test3/src/test3/Test1.java
@@ -2,6 +2,8 @@ package test3;
 
 public class Test1 {
 	public static void main(String[] args) {
+		System.out.println("sdfsdfsdfsdfsd");
+
 		System.out.println(111);
 		System.out.println(222);
 		System.out.println("aaa");
